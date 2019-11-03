da2ca307fb1f369601f78d5f3d82d766aa4fd4a6
刘肖 <15272491114@163.com>
2019-11-03 22:33:54 +0800

    初始化项目，配置线上环境变量，安装axios，element，sass，vuex

---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"prod"',
+  BASE_API: '"http://prod:8080"'
+}
---
+++ config/rc.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"rc"',
+  BASE_API: '"http://rc:8080"'
+}
