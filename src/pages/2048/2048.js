c71643013e73e9e12eb9be94be50efac1f89c4d7
liuxiao <15272491114@163.com>
2021-02-05 18:18:12 +0800

    2048开发

--- src/pages/2048/2048.js
+++ src/pages/2048/2048.js
@@ -1,16 +1,41 @@
 let Chess = function (options) {
   this.options = options
+
+  this.chessModel = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]
+
   // 单个棋子的宽度
-  this.chessPiecesWidth = (options.width - 60) / options.rows
+  this.chessPiecesWidth = (options.width - ((options.rows + 1) * 10)) / options.rows
   // 单个棋子的高度
-  this.chessPiecesHeight = (options.height - 60) / options.cols
+  this.chessPiecesHeight = (options.height - ((options.cols + 1) * 10)) / options.cols
 
   this.chessContainer = document.querySelector(this.options.el)
 
   this.initChessStyle()
   this.initBaseChessPiece()
+
+  let initX = Math.floor(Math.random() * 4)
+  let initY = Math.floor(Math.random() * 4)
+
+  /* eslint-disable */
+  new Marker({x: initX, y: initY, value: 2, width: this.chessPiecesWidth, height: this.chessPiecesHeight, chess: this.chessContainer})
+
+  // 根据棋盘数据模型渲染棋子实例
+  for (let y = 0; y < this.chessModel.length; y++) {
+    for (let x = 0; x < this.chessModel.length; x++) {
+      if (this.chessModel[y][x] !== 0) {
+        /* eslint-disable */
+        new Marker({x: x, y: y, value: this.chessModel[y][x], width: this.chessPiecesWidth, height: this.chessPiecesHeight, chess: this.chessContainer})
+      }
+    }
+  }
 }
 
+// 初始化棋盘
 Chess.prototype.initChessStyle = function () {
   this.chessContainer.style.width = this.options.width + 'px'
   this.chessContainer.style.height = this.options.height + 'px'
@@ -18,8 +43,11 @@ Chess.prototype.initChessStyle = function () {
   this.chessContainer.className = 'chessboard-container'
 }
 
+// 初始化背景棋子
 Chess.prototype.initBaseChessPiece = function () {
   let total = this.options.rows * this.options.cols
+
+  this.chessContainer.innerHTML = ''
 
   for (let i = 0; i < total; i++) {
     let chessPiece = document.createElement('div')
@@ -31,6 +59,26 @@ Chess.prototype.initBaseChessPiece = function () {
 
     this.chessContainer.appendChild(chessPiece)
   }
+}
+
+let Marker = function (markerOptions) {
+  this.x = markerOptions.x
+  this.y = markerOptions.y
+  this.value = markerOptions.value
+
+  let div = document.createElement('div')
+
+  div.innerHTML = markerOptions.value
+
+  div.style.width = markerOptions.width + 'px'
+  div.style.height = markerOptions.height + 'px'
+  div.style.lineHeight = markerOptions.height + 'px'
+  div.style.top = (this.y * markerOptions.height) + (this.y + 1) * 10 + 'px'
+  div.style.left = (this.x * markerOptions.width) + (this.x + 1) * 10 + 'px'
+
+  div.className = `marker marker-${markerOptions.value}`
+
+  markerOptions.chess.appendChild(div)
 }
 
 export default Chess
--- src/pages/2048/index.vue
+++ src/pages/2048/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
-    <div id="chessboard">
-    </div>
+    <div id="chessboard"></div>
   </div>
 </template>
 
@@ -11,20 +10,19 @@ import Chess from './2048.js'
 export default {
   data () {
     return {
-      itemCount: 25
     }
   },
   mounted () {
-    this.$nextTick(() => {
-      /* eslint-disable */
-      new Chess({
-        el: '#chessboard',
-        width: 500,
-        height: 500,
-        rows: 5,
-        cols: 5
-      })
+    /* eslint-disable */
+    let test = new Chess({
+      el: '#chessboard',
+      width: 500,
+      height: 500,
+      rows: 4,
+      cols: 4
     })
+
+    console.log(test)
   },
   methods: {
 
@@ -43,6 +41,7 @@ export default {
   padding: 10px;
 
   .chessboard-container {
+    position: relative;
     display: flex;
     flex-wrap: wrap;
     align-content: flex-start;
@@ -54,6 +53,57 @@ export default {
       background: #D6CDC4;
       margin-top: 10px;
       margin-left: 10px;
+    }
+
+    .marker {
+      position: absolute;
+      top: 0;
+      left: 0;
+      color: #ffffff;
+      text-align: center;
+      font-size: 50px;
+      font-weight: bold;
+      border-radius: 10px;
+    }
+
+    .marker-2 {
+      background: rgb(238, 228, 218);
+    }
+    .marker-4 {
+      background: rgb(236, 223, 203);
+    }
+    .marker-8 {
+      background: rgb(242, 177, 121);
+    }
+    .marker-16 {
+      background: rgb(245, 149, 99);
+    }
+    .marker-32 {
+      background: rgb(240, 127, 91);
+    }
+    .marker-64 {
+      background: rgb(246, 93, 59);
+    }
+    .marker-128 {
+      background: rgb(237, 206, 113);
+    }
+    .marker-256 {
+      background: rgb(233, 206, 100);
+    }
+    .marker-512 {
+      background: rgb(228, 201, 87);
+    }
+    .marker-1024 {
+      font-size: 40px;
+      background: rgb(176, 131, 173);
+    }
+    .marker-2048 {
+      font-size: 40px;
+      background: rgb(102, 105, 193);
+    }
+    .marker-4096 {
+      font-size: 40px;
+      background: rgb(157, 82, 168);
     }
   }
 }
